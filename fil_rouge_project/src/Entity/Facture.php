16f16d1fbb48e26dae4a3032c7783e329c4583a6
Aurelien <aurelien.bayard.pro@hotmail.com>
2024-11-29 11:27:17 +0100

    création table facture

---
+++ fil_rouge_project/src/Entity/Facture.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FactureRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: FactureRepository::class)]
+class Facture
+{
+    // #[ORM\Id]
+    // #[ORM\GeneratedValue]
+    // #[ORM\Column]
+    // private ?int $id = null;
+
+    #[ORM\Id]
+    #[ORM\Column]
+    protected ?int $numFacture = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    protected ?\DateTimeInterface $dateFacture = null;
+
+    // public function getId(): ?int
+    // {
+    //     return $this->id;
+    // }
+
+    public function getNumFacture(): ?int
+    {
+        return $this->numFacture;
+    }
+
+    public function setNumFacture(int $numFacture): static
+    {
+        $this->numFacture = $numFacture;
+
+        return $this;
+    }
+
+    public function getDateFacture(): ?\DateTimeInterface
+    {
+        return $this->dateFacture;
+    }
+
+    public function setDateFacture(\DateTimeInterface $dateFacture): static
+    {
+        $this->dateFacture = $dateFacture;
+
+        return $this;
+    }
+}
